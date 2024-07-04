85efd6b45380d131781e5b5dbca8fdcf981f4e4a
MohamedAmhal <mohammedamhal349@gmail.com>
2024-07-04 12:52:56 +0100

    upd

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,30 +12,29 @@ def canUnlockAll(boxes) :
     '''
     liste = []
     n = len(boxes)
-    if n == 1 :
-        return False
-    else:
+    
+    
+    for i in range(n):
+        if i == 0:
+            liste.append("YES")
+        else :
+            liste.append("NO")
+    while (liste.count("YES") != 0):
         for i in range(n):
-            if i == 0:
-                liste.append("YES")
-            else :
-                liste.append("NO")
-        while (liste.count("YES") != 0):
-            for i in range(n):
-                if liste[i] == "YES" :
-                    m = i
-                    break
-            r = len(boxes[m])
-            for j in range(r):
-                try :
-                    index = boxes[m][j]
-                    if liste[index] == "NO" :
-                        liste[index] = "YES"
-                except :
-                    continue
-            liste[m] = "YES AND CHECKED !"
-        for i in range(n):
-            if liste[i] == "NO" :
-                return False
-        
-        return True
+            if liste[i] == "YES" :
+                m = i
+                break
+        r = len(boxes[m])
+        for j in range(r):
+            try :
+                index = boxes[m][j]
+                if liste[index] == "NO" :
+                    liste[index] = "YES"
+            except :
+                continue
+        liste[m] = "YES AND CHECKED !"
+    for i in range(n):
+        if liste[i] == "NO" :
+            return False
+            
+    return True
