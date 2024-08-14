4b4ef713ac21e56799fa67e4cd9f423f38f2eb8e
MohamedAmhal <mohammedamhal349@gmail.com>
2024-08-14 22:22:33 +0100

    updating:

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,30 +7,27 @@ doc doc doc doc doc doc !
 '''
 
 
-def transpose(matrix, n):
-    '''
-    transpose a matrix 2d
-    n * n size !!!
-    '''
-    for i in range(n):
-        for j in range(n):
-            if i != j and i > j:
-                a = matrix[i][j]
-                matrix[i][j] = matrix[j][i]
-                matrix[j][i] = a
-
-
 def rotate_2d_matrix(matrix):
-    '''
-    the function that relate the matrix
-    of the size n * n!
-    '''
-    n = len(matrix)
-    transpose(matrix, n)
-    print(matrix)
-    for i in range(n):
-        for j in range(n):
-            if j <= (n / 2 - 1):
-                a = matrix[i][j]
-                matrix[i][j] = matrix[i][n - 1 - j]
-                matrix[i][n - 1 - j] = a
+    """Rotates an m by n 2D matrix in place.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
